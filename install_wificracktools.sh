d2631a2a76746919a979dde493505efd3547ecb2
Phillip Housden <philliphousden@protonmail.com>
2021-02-05 07:40:43 -0500

    Update install_wificracktools.sh

--- install_wificracktools.sh
+++ install_wificracktools.sh
@@ -18,7 +18,7 @@ if [ "$EUID" -ne 0 ]; then
 	exit
 fi
 
-apt update && apt install -y zlib1g-dev libcurl4-openssl-dev libssl-dev build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils
+apt update && apt install -y zlib1g-dev libcurl4-openssl-dev libssl-dev build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils git
 
 git clone https://github.com/ZerBea/hcxtools.git
 cd hcxtools/
